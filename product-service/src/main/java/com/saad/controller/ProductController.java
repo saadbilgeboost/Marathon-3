9f435fce34942e865851236015989ff219ebb123
saadbilgeboost <saad.ce.1993@gmail.com>
2022-10-31 16:22:13 +0300

    FirstCommit

--- product-service/src/main/java/com/saad/controller/ProductController.java
+++ product-service/src/main/java/com/saad/controller/ProductController.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 
 import static com.saad.constants.ApiUrls.*;
